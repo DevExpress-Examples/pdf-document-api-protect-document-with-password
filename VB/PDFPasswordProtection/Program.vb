647491b1cf3e51ff2c3da26d3e4d9c61145b15f1
DevExpressExampleBot <fakeEmail>
2022-02-01 16:36:50 +0300

    Source auto update [skip ci]

--- VB/PDFPasswordProtection/Program.vb
+++ VB/PDFPasswordProtection/Program.vb
@@ -1,32 +1,27 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 
 Namespace PDFPasswordProtection
+
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
-            Using pdfDocumentProcessor As New PdfDocumentProcessor()
-
+        Shared Sub Main(ByVal args As String())
+            Using pdfDocumentProcessor As DevExpress.Pdf.PdfDocumentProcessor = New DevExpress.Pdf.PdfDocumentProcessor()
                 ' Load a PDF document.
                 pdfDocumentProcessor.LoadDocument("..\..\Demo.pdf")
-
                 ' Specify printing, data extraction, modification, and interactivity permissions. 
-                Dim encryptionOptions As New PdfEncryptionOptions()
-                encryptionOptions.PrintingPermissions = PdfDocumentPrintingPermissions.Allowed
-                encryptionOptions.DataExtractionPermissions = PdfDocumentDataExtractionPermissions.NotAllowed
-                encryptionOptions.ModificationPermissions = PdfDocumentModificationPermissions.DocumentAssembling
-                encryptionOptions.InteractivityPermissions = PdfDocumentInteractivityPermissions.Allowed
-
+                Dim encryptionOptions As DevExpress.Pdf.PdfEncryptionOptions = New DevExpress.Pdf.PdfEncryptionOptions()
+                encryptionOptions.PrintingPermissions = DevExpress.Pdf.PdfDocumentPrintingPermissions.Allowed
+                encryptionOptions.DataExtractionPermissions = DevExpress.Pdf.PdfDocumentDataExtractionPermissions.NotAllowed
+                encryptionOptions.ModificationPermissions = DevExpress.Pdf.PdfDocumentModificationPermissions.DocumentAssembling
+                encryptionOptions.InteractivityPermissions = DevExpress.Pdf.PdfDocumentInteractivityPermissions.Allowed
                 ' Specify the owner and user passwords for the document.  
                 encryptionOptions.OwnerPasswordString = "OwnerPassword"
                 encryptionOptions.UserPasswordString = "UserPassword"
-
                 ' Specify the 256-bit AES encryption algorithm.
-                encryptionOptions.Algorithm = PdfEncryptionAlgorithm.AES256
-
+                encryptionOptions.Algorithm = DevExpress.Pdf.PdfEncryptionAlgorithm.AES256
                 ' Save the protected document with encryption settings.  
-                pdfDocumentProcessor.SaveDocument("..\..\ProtectedDocument.pdf", New PdfSaveOptions() With {.EncryptionOptions = encryptionOptions})
+                pdfDocumentProcessor.SaveDocument("..\..\ProtectedDocument.pdf", New DevExpress.Pdf.PdfSaveOptions() With {.EncryptionOptions = encryptionOptions})
             End Using
         End Sub
     End Class
 End Namespace
-
