985e2a8fc80a6f961139d9dff2fa4a5dcd9123b2
DevExpressExampleBot <fakeEmail>
2025-02-20 19:05:41 +0400

    Source auto update [skip ci]

--- VB/PDFPasswordProtection/Program.vb
+++ VB/PDFPasswordProtection/Program.vb
@@ -1,13 +1,14 @@
 Imports DevExpress.Pdf
+Imports System.Diagnostics
 
 Namespace PDFPasswordProtection
 
     Friend Class Program
 
         Shared Sub Main(ByVal args As String())
-            Using pdfDocumentProcessor As New PdfDocumentProcessor()
+            Using pdfDocumentProcessor As DevExpress.Pdf.PdfDocumentProcessor = New DevExpress.Pdf.PdfDocumentProcessor()
                 ' Load a PDF document.
-                pdfDocumentProcessor.LoadDocument("..\..\Demo.pdf")
+                pdfDocumentProcessor.LoadDocument("..\..\..\Demo.pdf")
                 ' Specify printing, data extraction, modification, and interactivity permissions. 
                 Dim encryptionOptions As DevExpress.Pdf.PdfEncryptionOptions = New DevExpress.Pdf.PdfEncryptionOptions()
                 encryptionOptions.PrintingPermissions = DevExpress.Pdf.PdfDocumentPrintingPermissions.Allowed
@@ -20,8 +21,10 @@ Namespace PDFPasswordProtection
                 ' Specify the 256-bit AES encryption algorithm.
                 encryptionOptions.Algorithm = DevExpress.Pdf.PdfEncryptionAlgorithm.AES256
                 ' Save the protected document with encryption settings.  
-                pdfDocumentProcessor.SaveDocument("..\..\ProtectedDocument.pdf", New DevExpress.Pdf.PdfSaveOptions() With {.EncryptionOptions = encryptionOptions})
+                pdfDocumentProcessor.SaveDocument("..\..\..\ProtectedDocument.pdf", New DevExpress.Pdf.PdfSaveOptions() With {.EncryptionOptions = encryptionOptions})
             End Using
+
+            Process.Start(New ProcessStartInfo("..\..\..\ProtectedDocument.pdf") With {.UseShellExecute = True})
         End Sub
     End Class
 End Namespace
